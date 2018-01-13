798144d092f428d7c2b38488f693339d8de869fb
Rubèn Nieto <ruben.np@me.com>
2018-01-13 22:59:43 +0100

    Installed and running homestead/laravel

--- vuebnb/resources/views/welcome.blade.php
+++ vuebnb/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    vuebnb
                 </div>
 
                 <div class="links">
